f6fafc23505f600fda056b9c086070d6ec24591d
Dmitriy <vavilon2@gmail.com>
2018-09-01 21:17:09 +0300

    Settings dev folder
     -create admin html
     -create .htaccess for Apache
     -move start data from index.html to api.js

---
+++ dev/api.js
@@ -0,0 +1,130 @@
+var _APP_DOMAIN ={
+
+   "active":{
+     "host":"siteo.top",
+     "path":"\/",
+     "lang":"en",
+     "state":"reserved"
+     }
+   };
+
+
+var _APP_INSTANCE = {
+ data:{
+   _id:152710249031417,
+   pid:"7rL5Q4LRdk",
+   name:"Gotothegoal",
+   title:"Title Website",
+   slogan:"Slogan of Website",
+   actionText:"Action Text",
+   description:"Short description ",
+   domain:"",
+   templates_id:"1532775890",
+   countryAlpha2:"UA",
+   lang:"uk",
+   design:false
+ },
+ "design":{
+   "theme":{
+     "name":"theme",
+     "colors":{
+       "primary":"#64b5f6",
+       "secondary":"#424242",
+       "accent":"#82B1FF",
+       "error":"#FF5252",
+       "warning":"#FFC107",
+       "info":"#2196F3",
+       "success":"#4CAF50"}
+     },
+     "AppDrawer":{
+       "name":"AppDrawer",
+       "props":{},
+       "class":[],
+       "children":[
+         {
+           "name":"AppDrawerHeader",
+           "props":{},
+           "class":[]
+         },
+         {
+           "name":"AppDrawerMenu",
+           "props":{},
+           "class":[]
+         }
+       ]
+     },
+     "AppToolbar":{
+       "name":"AppToolbar",
+       "props":{"dark":true},
+       "class":["primary"],
+       "children":[
+         {
+           "name":"AppToolbarSideIcon",
+           "props":{},
+           "class":[]
+         },
+         {
+           "name":"AppToolbarLogoIcon",
+           "props":{"color":"primary lighten-5"},
+           "class":[]
+         },
+         {
+           "name":"AppToolbarTitle",
+           "props":{},
+           "class":["mr-5","display-1"]
+         },
+         {
+           "name":"VSpacer",
+           "props":{},
+           "class":[]
+         },
+         {
+           "name":"AppToolbarItems",
+           "props":{},
+           "class":[]
+         },
+         {
+           "name":"AppToolbarAction",
+           "props":{},
+           "class":[]
+         }
+       ]
+     },
+     "AppFooter":{
+       "name":"AppFooter",
+       "props":{},
+       "class":[]
+     },
+     "AppContent":{
+       "name":"AppContent",
+       "props":{},
+       "class":[]
+     },
+     "AppAction":{
+       "name":"AppAction",
+       "props":{},
+       "class":[]
+     }},
+
+ "menu":[
+   {
+     "i":"user-sett",
+     "id":"main",
+     "path":"\/",
+     "title":"\u0413\u043e\u043b\u043e\u0432\u043d\u0430"
+   },
+   {
+     "i":"user-sett",
+     "id":"services",
+     "path":"\/services",
+     "title":"Услуги"
+   },
+   {
+     "i":"user-sett",
+     "id":"location",
+     "path":"\/location",
+     "title":"Адрес"
+   }
+  ]
+ };
+ var _PRERENDER =null;
