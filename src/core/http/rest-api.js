eda3d5514466bfa864e513b20cc67a0cec895e6a
Dmitriy <vavilon2@gmail.com>
2018-09-18 20:05:25 +0300

    Leave Only one axios file

--- src/core/http/rest-api.js
+++ src/core/http/rest-api.js
@@ -1,20 +1,47 @@
-
-
-
-import axios from  './interceptors.js';
+import axios from 'axios';
 import {configApps} from '../config';
-//var params = {};
-/*if (_GC.LANG_ACTIVE != _GC.LANG_DEFAULT) {
-    params = {lang: _GC.LANG_ACTIVE};
-} else {
-  params = {}
-}
-*/
+
+
+  // Add a request interceptor
+  axios.interceptors.request.use(
+  function (config) {
+        console.log('я выполняю запрос');
+        console.log(config);
+      //   nanobar.go(30);  deleted because for loaded content need use diferent loader nanobar only for routing
+    //    Vue.$Progress.start();
+        return config;
+    },
+  function (error) {
+
+    // Do something with request error
+    //  return Promise.reject(error);
+    }
+  );
+
+  // Add a response interceptor
+  axios.interceptors.response.use(
+  function (response) {
+      // Do something with response data
+      console.log('I am get response ');
+      console.log( response);
+
+      //  console.log(response);
+      if (response && response.status==200 && typeof (response.data)!=='object') {
+          throw  {response:response};
+      }
+      return response;
+  }, function (error) {
+    //  console.log(error);
+      console.log(error.response);
+      //nanobar.go(100);
+  // Do something with response error
+  //  return error;
+     return Promise.reject(error);
+  });
+
 var api = axios.create({
       baseURL: configApps.API_URL,
-      withCredentials: true,
-      //headers: {'common': { 'Authorization':"Bearer sdfsdfsdfsdf23424s" }},
-      //  params:params
+
 });
 
 
