4a199dec5da3d905cd5041a63576cdbbf9843d7e
Dmitriy <vavilon2@gmail.com>
2018-10-09 16:56:44 +0300

    Delete comment render  function from AppForm

--- src/template/components/Elements/AppForm/mixins/render.js
+++ src/template/components/Elements/AppForm/mixins/render.js
@@ -64,7 +64,7 @@ export default {
       var self = this;
       return h('v-layout', {
           props: {row: true, wrap: true },
-          class: ['my-3'], 
+          class: ['my-3'],
           directives: [
             {
               name: 'show',
@@ -158,111 +158,6 @@ export default {
         ]
       )
   }
-  /**
-    RENDER FUNCTION FOR FORM
 
-     render(h) {
-
-           <v-card-text>
-           {this.$_LocalMessages_render(h)}
-           <div class="pa-3"  >
-
-                {
-                  _Values(this.formStructure).map(function(el) {
-
-                    var myinput =function (ev) {
-                        el.value = ev;
-
-                        if (el.trim) {
-                            el.value = ev.trim();
-                            //console.log(el.value);
-                        }
-
-                    }
-
-
-                    switch (el.component) {
-
-
-                    case 'AppInputRecaptcha' :
-                                if (self.formActive) {
-                                    return  <div class="mt-2 mb-2">
-
-                                        <google-recaptcha
-                                            status={el.reload}
-                                            on-changeCaptcha={myinput}
-                                        ></google-recaptcha>
-                                        <v-alert  outline color="error"  value={el.state}>
-                                            {el.feedback}
-                                        </v-alert>
-                                 </div>
-                               }
-                              break;
-                      case 'AppInputText':
-                      case 'AppSelect':
-                      case 'AppInputDate':
-                      case 'AppInputPhone':
-                      case 'AppCheckbox':
-                      case 'AppSelectCountries':
-                      case 'AppInputPassword':
-                      //  console.log(el.type);
-                        if (el.renderFunction) {
-                          return el.renderFunction(h, el, self);
-                        } else {
-                          return   self.renderInput(h, el);
-                        }
-
-                        break;
-
-                      case 'submit':
-
-                            return <v-layout row wrap v-show={(!self.isLoaderActive&&self.buttonSubmit)}>
-
-                                <v-flex xs10>
-                                    <v-btn  disabled={!self.formActive}  on-click={self.onSubmit} color="default" large>{el.label? el.label: "send"}</v-btn>
-                                </v-flex>
-                                <v-flex xs2>
-                                <v-tooltip
-                                   top
-                                 >
-                                   <v-btn
-                                     icon
-                                     disabled={!self.formActive}
-                                     on-click={()=>{ self.leaveform = self.clearForm }  }
-                                     slot="activator"
-                                   >
-                                   <app-icon name="form-clear"></app-icon>
-                                  </v-btn>
-                                 <span>Reset form</span>
-                               </v-tooltip>
-                                </v-flex>
-
-                              </v-layout>
-                      break;
-                      default:
-
-                    }
-
-                  })
-                }
-
-                  <app-pulse-loader loading={this.isLoaderActive} ></app-pulse-loader>
-
-                  {self.renderFooter(h)}
-
-           </div>
-             {
-
-               self.renderConfirm()
-
-            }
-            </v-card-text>
-           </v-card>
-
-
-        return a;
-
-    }
-*/
 
 }
