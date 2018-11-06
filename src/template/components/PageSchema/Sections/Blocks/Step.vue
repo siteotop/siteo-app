8aced9bd364e620ad8c1fe0aaa45e0937add00d6
Dmitriy <vavilon2@gmail.com>
2018-11-06 14:48:40 +0200

    Remake StepByStep

---
+++ src/template/components/PageSchema/Sections/Blocks/Step.vue
@@ -0,0 +1,32 @@
+<template functional>
+  <v-card>
+      <v-img
+        aspect-ratio="1.77"
+        class="white--text"
+        :src="props._m"
+      >
+        <v-container fill-height fluid>
+          <v-layout fill-height>
+            <v-flex xs12 align-end flexbox>
+               <span class="headline"><v-avatar color="primary">{{(i+1)}}</v-avatar> {{props._t}}</span>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-img>
+      <v-card-title v-if="props._s">
+        <span class="grey--text">{{props._s}}</span>
+      </v-card-title>
+      <v-card-text>
+        {{props._d}}
+      </v-card-text>
+    </v-card>
+</template>
+<script>
+import BTitleProps from './_extends/b-title-props.js';
+
+export default {
+   extends: BTitleProps,
+
+}
+
+</script>
