c29272b636c1efd6358a43affe2b4a58cc40950c
Dmitriy <vavilon2@gmail.com>
2018-11-06 17:40:38 +0200

    Create BLocation

---
+++ src/template/components/PageSchema/Sections/Blocks/Location.vue
@@ -0,0 +1,33 @@
+<template>
+<v-card >
+ <v-card-title class="headline pb-0">
+  <AppIcon name="si-locations"></AppIcon>
+  {{location.title}}
+  <v-spacer></v-spacer>
+ </v-card-title>
+ <v-card-text>
+    <span>{{location.city}}, {{location.street}}, {{location.street_n}}
+    </span>
+    <v-spacer></v-spacer>
+    {{location.zipcode}}
+ </v-card-text>
+ <v-card-actions>
+  <AppAction
+    block
+    color="red"
+    outline
+    target="_blank"
+    siicon="si-googlemaps" :href="'https://www.google.com/maps/dir/?api=1&destination='+location.lat+','+location.lng" _at="Direction in Google Maps" >
+  </AppAction>
+ </v-card-actions>
+ <v-card-actions>
+  <AppAction
+    block
+    outline
+    target="_blank"
+    siicon="si-locations" :href="'geo:'+location.lat+','+location.lng" _at="Open Default Map App" >
+  </AppAction>
+ </v-card-actions>
+</v-card>
+
+</template>
---
+++ src/template/components/PageSchema/Sections/Items/Locations.vue
@@ -0,0 +1,23 @@
+<template functional>
+<v-container class="grid-list-xs pa-0">
+  <v-layout row wrap >
+    <v-flex xs12 v-for="(location, index) in parent.$store.state.APP_INSTANCE.locations" :key="index">
+  
+      </v-flex>
+   </v-layout>
+</v-container>
+</template>
+
+<script>
+//https://stackoverflow.com/questions/9688607/how-to-open-a-mobile-devices-map-app-when-a-user-clicks-on-a-link
+// Google Maps link https://developers.google.com/maps/documentation/urls/guide#search-action
+/**
+<AppAction
+  target="_blank"
+  siicon="si-locations" :href="'geo:'+location.lat+','+location.lng" _at="Open Map App" >
+</AppAction>
+*/
+export default {
+
+}
+</script>
