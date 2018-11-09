cd0e245512cd861f67594665e2a3f0bc208f73d2
Dmitriy <vavilon2@gmail.com>
2018-11-09 16:27:12 +0200

    Remove folders with icons

--- src/core/icons/js/bell.js
+++
@@ -1,9 +0,0 @@
-import Icon from 'vue-awesome/components/Icon.vue'
-//https://materialdesignicons.com/icon/bell
-Icon.register({
-  'bell': {
-    width: 24,
-    height: 24,
-    d: 'M12,22A2,2 0 0,0 14,20H10A2,2 0 0,0 12,22M18,16V11C18,7.93 16.36,5.36 13.5,4.68V4A1.5,1.5 0 0,0 12,2.5A1.5,1.5 0 0,0 10.5,4V4.68C7.63,5.36 6,7.92 6,11V16L4,18V19H20V18L18,16Z'
-  }
-})
