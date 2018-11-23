04835f4404794814a455a23b23949931346a9f61
Dmitriy <vavilon2@gmail.com>
2018-11-23 09:56:07 +0200

    Remove AppVBtnMobile component

--- src/core/components/index.js
+++ src/core/components/index.js
@@ -1,7 +1,6 @@
 // Global Elements
 export {default as AppAction} from './Elements/AppAction/';
 export {default as AppIcon} from './Elements/AppIcon';
-export {default as AppVBtnMobile} from './Elements/AppVBtnMobile.js';
 export {default as AppPulseLoader} from './Elements/AppPulseLoader.vue';
 export {default as AppMessagesBlock} from './Elements/AppMessagesBlock.js';
 export {default as AppForm} from './Elements/AppForm';
