8c4c1b459ebab53e75db5c84f5da6f512c925b1a
Dmitriy <vavilon2@gmail.com>
2019-02-01 21:18:42 +0200

    Add html-webpack-plugin

--- ssr/configs.js
+++ ssr/configs.js
@@ -1,9 +1,15 @@
 module.exports= {
+    // token for API
   public_token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1YzBlNmZmZTAxN2UxIiwiYXVkIjoiY29yZV9zZXJ2ZXJfYXBwIiwic3ViIjoiIiwiaXNzIjoiYXV0aC12MS4wIiwidHQiOjEsImlwIjoiMTI3LjAuMC4xIn0.HCQ4RoOOXEo9-IjYJmESLgWy-aDMwsUbHRbo2VMOHuY',
+
+  // example  https://api.siteo.top/api/v1
   api_url: 'https://api.siteo.top/api/v1',
+  // siteo id for example 152710249031417
   siteo_id: '152774684316952',
   path:"",
+  // website host https://siteo.top
   host: process.env.NODE_ENV =='production'? "https://siteo.top":'http://localhost:8080',
   lang: 'en',
-  host_static: process.env.NODE_ENV =='production'? "https://siteo.top/dist/":'http://localhost:8080/dist/',
+  // host static curent or 
+  host_static: process.env.NODE_ENV =='production'? "https://siteo.top/dist/":'/',
 }
