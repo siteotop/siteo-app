f228267d7da7d91dec812fdddfd964f0d76cf34e
DmytroDiachuk <vavilon2@gmail.com>
2019-04-23 13:13:41 +0300

    Add default props to SdDrw

--- src/core/components/Structure/SdDrw.vue
+++ src/core/components/Structure/SdDrw.vue
@@ -1,5 +1,9 @@
 <template functional>
-<v-navigation-drawer app fixed
+<v-navigation-drawer
+  app
+  fixed
+  :disable-route-watcher="true"
+  :disable-resize-watcher="true"
  :value="parent.$store.state.drawer"
  @input="(value)=>parent.$store.state.drawer = value">
   <StChildrenHelper v-for="(element, index) in props.chldrn||[]"
