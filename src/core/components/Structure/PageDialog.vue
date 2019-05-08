4f45723f6cc490d9cb36c843cd79ece32120dfea
DmytroDiachuk <vavilon2@gmail.com>
2019-05-08 09:15:24 +0300

    PageDialog with slots

--- src/core/components/Structure/PageDialog.vue
+++ src/core/components/Structure/PageDialog.vue
@@ -1,34 +1,5 @@
-<template>
-<v-dialog
-  v-model="dialog"
-  v-bind="{
-      dark: cnf.d,
-      light: cnf.l,
-      fullWidth: cnf.f,
-      fullscreen: cnf.e,
-      hideOverlay: cnf.h,
-      lazy: cnf.z,
-      maxWidth:cnf.m,
-      origin: cnf.o,
-      persistent: cnf.p,
-      scrollable: cnf.s,
-      transition: cnf.r,
-      width: cnf.w,
-    }"
->
-
-<template v-if="chldrn.length" #activator="{ on }">
-  <RBt v-if="chldrn[0]._n=='RBt'"  v-on="on" v-bind="{cntnt:chldrn[0]._d, cnf: chldrn[0]._p }">
-  </RBt>
- </template>
-
- <template v-if="chldrn.length&&chldrn[1]" #default>
-   <StChildrenHelper v-if="chldrn[1]._n=='PCd'" :element="chldrn[1]">
-   </StChildrenHelper>
-</template>
-</v-dialog>
-</template>
 <script>
+//
 export default {
   props: {
     chldrn: {
@@ -48,8 +19,53 @@ export default {
     return {
       dialog:false
     }
-  }
+  },
 
+
+  render(h) {
+     var cnf = this.cnf;
+
+
+     var scopedSlots = {};
+     var children = [];
+
+     this.chldrn.map(function(element) {
+       if (element._n == 'RBt') {
+         scopedSlots['activator'] = function (ref) {
+           return h('StChildrenHelper', {props: {element:element}, on:ref.on });
+         }
+       } else {
+         children =[  h('StChildrenHelper', {props: {element:element}})];
+       }
+     })
+
+     return   h('v-dialog', {
+        props: {
+            dark: cnf.d,
+            light: cnf.l,
+            fullWidth: cnf.f,
+            fullscreen: cnf.e,
+            hideOverlay: cnf.h,
+            lazy: cnf.z,
+            maxWidth:cnf.m,
+            origin: cnf.o,
+            persistent: cnf.p,
+            scrollable: cnf.s,
+            transition: cnf.r,
+            width: cnf.w
+          },
+        model: {
+            value: self.dialog,
+            callback: function($$v) {
+               self.dialog = $$v
+            },
+            expression: "dialog"
+        },
+        scopedSlots: scopedSlots
+
+      }, children)
+  }
 }
+
 
 </script>
