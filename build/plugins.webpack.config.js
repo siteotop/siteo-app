5884557bcff9b3e33481cc16f577da79882e9614
DmytroDiachuk <vavilon2@gmail.com>
2019-06-07 11:36:46 +0300

    chore(plugins): Separate webpack file for plugins

--- build/plugins.webpack.config.js
+++ build/plugins.webpack.config.js
@@ -1,22 +1,48 @@
 
 const path = require('path');
-const merge = require('webpack-merge')
 const webpack = require('webpack');
-//const nodeExternals = require('webpack-node-externals')
-const baseConfig = require('./base.webpack.config.js')
 const DIR_RESOURCE=require('./helper/dirResource')('plugins', process.env.NODE_ENV);
-
-baseConfig.plugins.push( new webpack.DefinePlugin({
-  'process.env': {
-     NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-  }
-}));
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 
-module.exports = merge(baseConfig, {
+module.exports = {
+  mode:  process.env.NODE_ENV,
+  devtool:  process.env.NODE_ENV ==  'development' ? "source-map" : false,
+  watch: process.env.NODE_ENV ==  'development',  // наблюдение за изменяемыми файлами
+  watchOptions: {
+      aggregateTimeout: 500,
+      ignored: /node_modules/,
+      poll: 1000
+ },
+
+ optimization: {
+   minimizer: [
+     /**
+       it work on this options https://github.com/mishoo/UglifyJS2/tree/harmony#minify-options
+     */
+     new UglifyJsPlugin({
+       uglifyOptions: {
+         output: {
+           comments : false // remove all comments
+         },
+
+         compress: {
+             drop_console: true // drop console call
+
+         }
+       }
+     }
+     )
+
+   ],
+ },
+
   // Укажите точку входа серверной части вашего приложения
+
+
   entry: {
-    
+
     'instagram-post':   './src/plugins/instagram-post',
     'forms':   './src/plugins/forms',
     'structure-admin':   './src/plugins/structure-admin',
@@ -34,5 +60,76 @@ module.exports = merge(baseConfig, {
 		libraryTarget: "umd",
     libraryExport: 'default',
 
-  }
-})
+  },
+
+  plugins: [
+    /**Vue Loader */
+    new VueLoaderPlugin(),
+
+    new webpack.DefinePlugin({
+      'process.env': {
+         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      }
+    })
+
+  ],
+
+  module: {
+
+    rules: [
+
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        // this will apply to both plain `.js` files
+        // AND `<script>` blocks in `.vue` files
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          include: [path.resolve(__dirname, '../src')]
+        },
+
+
+        /**
+          was created  by this documentation https://webpack.js.org/loaders/sass-loader/
+          and Vue Loader  https://vue-loader.vuejs.org/migrating.html#css-extraction
+
+        */
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+          //  process.env.NODE_ENV !== 'production'? 'vue-style-loader':undefined,
+            {
+            loader: 'css-loader',
+            options: {
+                // enable CSS Modules
+                modules: true,
+                // customize generated class names
+                localIdentName: '[local]_[hash:base64:8]'
+              }
+            },
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
+
+        /**
+          like above config
+        */
+        {
+          test: /\.styl$/,
+          use: [
+
+            // https://vue-loader.vuejs.org/guide/extract-css.html#webpack-4
+          //  process.env.NODE_ENV !== 'production'? 'vue-style-loader':undefined,
+            'css-loader',
+            'postcss-loader',
+            'stylus-loader',
+
+          ]
+        },
+
+  ]
+}
+};
