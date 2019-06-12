23b21fc41cfa6f78bbd0d73d0687b57d181cca22
DmytroDiachuk <vavilon2@gmail.com>
2019-06-12 16:55:30 +0300

    feat(plugin-forms): Update AppFieldWorldApi
    - компоненту передається лінк на api (not store)

--- src/plugins/forms/AppForm/Fields/AppFieldWorldApi.vue
+++ src/plugins/forms/AppForm/Fields/AppFieldWorldApi.vue
@@ -1,5 +1,11 @@
 <template>
- <v-select v-bind="vComp" v-model="valueData" :items="items">
+ <v-select
+    v-bind="vComp"
+    :loading="loading"
+    v-model="valueData"
+    :items="items"
+
+    >
  </v-select>
 </template>
 
@@ -14,27 +20,65 @@ export default {
       type: Object
     },
 
+    // when use siteo API
+    internalApi: {
+      type: String,
+      default:''
+    },
+
+    // when use link others api
+    externalApi: {
+      type: String,
+      default:''
+    },
+
 
   },
 
   data() {
       return  {
+        loading: true,
         items: []
       }
   },
 
   mounted() {
-    console.log('fetch api selecteble');
-    var self = this;
 
+    console.log('fetch api selecteble');
+
+    this.getResource();
     // create axios getting result for every filter
-    setTimeout(function(){
+  /**  setTimeout(function(){
       self.items = [
         {value:'ua', text: 'Ukraine'},
         {value:'ru', text: 'Russia'},
         {value:'pl', text: 'Poland'},
     ];
-  }, 300)
+  }, 300)**/
+},
+
+  methods:{
+      getResource() {
+         var self = this;
+         if (this.internalApi) {
+              var  config = {
+                 url: this.internalApi
+              }
+              this.$store.dispatch('callAPI', config).then(result=>{
+                  console.log(result);
+                  if (result.status==200&&result.data) {
+                      self.items =result.data.items||[];
+                  }
+                  self.loading = false;
+              }).catch(error=>{
+                 console.log(error);
+                  self.loading = false;
+              })
+         } else {
+            self.loading = false;
+         }
+
+      }
   }
 }
 </script>
