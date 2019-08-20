38494c67c3b133ee55ed838101ec2bada810bace
Dmytro Diachuk <vavilon2@gmail.com>
2019-08-20 12:06:32 +0300

    Fix(adsense): Fix problem with adsense (Block did not show)

--- src/core/components/Structure/PAdsense.vue
+++ src/core/components/Structure/PAdsense.vue
@@ -1,7 +1,7 @@
 <template>
 <div ref="block" :style="widthBlock">
 
-  <ins v-if="this.configAdsense" class="adsbygoogle"
+  <ins v-if="configAdsense" class="adsbygoogle"
     v-bind="configAdsense"
   >
   </ins>
@@ -41,7 +41,7 @@ export default {
   data() {
     return {
       widthBlock: {},
-      configAdsense: {}
+      configAdsense: false
       }
 
   },
@@ -50,42 +50,53 @@ export default {
   mounted() {
     //console.log(this.$refs.skills.parentNode.parentElement.width);
     //console.log(document.getElementById(this.idGenerated).parentElement.offsetWidth)
-    var width = this.$refs.block.parentElement.offsetWidth;
-    console.log(width)
 
-    this.widthBlock = {width: (width -20) + 'px' };
+    //console.log(this.$refs.block.parentElement);
+    //console.log(this.$refs.block.parentElement.offsetWidth);
+    //console.log(this.$refs.block.parentElement.clientWidth);
+    //console.log(window.getComputedStyle(this.$refs.block.parentElement).width);
+
+
+
+      var self=this;
+      var  createAd = function () {
+          var width = self.$refs.block.parentElement.offsetWidth;
+          console.log(width)
+
+          self.widthBlock = {width: (width -20) + 'px', border: '1px solid red' };
+        self.configAdsense = {
+           'display': {
+             'style':"display:block;",
+             'data-ad-format':"auto",
+             'data-full-width-responsive':"true"
+           },
+           'inarticle': {
+              'style':"display:block; text-align:center;",
+              'data-ad-layout':"in-article",
+              'data-ad-format':"fluid",
+           },
+           'infeed': {},
+           'links': {},
+        }[self.adType];
+
+        var configes = self.$store.getters.getSiteoConfig('siteo-plugin-googleadsense');
+        if (configes) {
+          self.configAdsense['data-ad-client'] = configes.pub;
+          self.configAdsense['data-ad-slot'] = configes.pub['block_'+self.adType];
+
+          self.$root.$options.$script('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js').then(function(){
+            (adsbygoogle = window.adsbygoogle || []).push({})
+          });
+
+        } else {
+            self.configAdsense = false;
+        }
+
+      }
+    setTimeout(createAd, 10);
     //(window.adsbygoogle = window.adsbygoogle || []).push({})
-    this.configAdsense = {
-       'display': {
-         'style':"display:block;",
-         'data-ad-client':"ca-pub-3374933954829220",
-         'data-ad-slot':"8663939025",
-         'data-ad-format':"auto",
-         'data-full-width-responsive':"true"
-       },
-       'inarticle': {
-         'style':"display:block; text-align:center;",
-          'data-ad-layout':"in-article",
-          'data-ad-format':"fluid",
-       },
-       'infeed': {},
-       'links': {},
-    }[this.adType];
-
-    var configes = this.$store.getters.getSiteoConfig('siteo-plugin-googleadsense');
-    if (configes) {
-      this.configAdsense['data-ad-client'] = configes.pub;
-      this.configAdsense['data-ad-slot'] = configes.pub['block_'+this.adType];
-
-    } else {
-        this.configAdsense = false;
-    }
 
 
-
-    this.$root.$options.$script('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js').then(function(){
-      (adsbygoogle = window.adsbygoogle || []).push({})
-    });
 
 
 
