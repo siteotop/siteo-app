e708c5fae1ef4bc4c164e81e9d313aa64a607062
Dmytro Diachuk <vavilon2@gmail.com>
2019-11-07 06:51:52 +0200

    feat: resolve app  pathes in html text (PHtml)

--- src/core/components/Structure/PHtml.vue
+++ src/core/components/Structure/PHtml.vue
@@ -1,9 +1,40 @@
-<template functional>
-<div :class="data.class" v-html="props.cntnt.t">
+<template >
+<div v-html="cntnt.t">
 </div>
 </template>
 <script>
 export default {
+  props: ['cntnt'],
 
+  // на SSR сервері це не потрібно тому ми робимо це в mounted
+  mounted() {
+
+    const container = this.$el.querySelectorAll('a');
+    // https://ru.stackoverflow.com/questions/649396/%D0%A1%D0%BE%D0%B1%D1%8B%D1%82%D0%B8%D0%B5-onclick-%D0%B4%D0%BB%D1%8F-%D0%B2%D1%81%D0%B5%D1%85-%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D0%BE%D0%B2-%D0%BF%D0%BE-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-%D1%82%D0%B5%D0%B3%D0%B0-%D0%B8%D0%BB%D0%B8-%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B0
+    var self = this;
+    for (var i = 0; i < container.length; i++) {
+
+      container[i].onclick = function(e){
+        e.preventDefault();
+        const url = this.href;
+        const host = self.$store.getters['CORE_HOST'];
+        const position = url.indexOf(host, 0);
+
+        if (position ==-1) {
+            // if URL isn't path for app
+            window.location = url;
+            //  window.open('', '_blank');
+        } else {
+            // check for app path;
+            let path = url.slice(host.length);
+            if (!path) {
+              path = '/';
+            }
+            self.$router.push({path:path});
+        }
+      };
+    }
+
+  }
 }
 </script>
