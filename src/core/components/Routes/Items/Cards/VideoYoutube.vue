bf938b1c4633e3984b85e37b6f1677b695bdd50c
Dmytro Diachuk <vavilon2@gmail.com>
2019-11-11 19:10:32 +0200

    feat: Add new component for video

---
+++ src/core/components/Routes/Items/Cards/VideoYoutube.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-dialog v-model="modalValue" max-width="700">
+    <template v-slot:activator="{ on }">
+      <v-btn  fab small v-on="on">
+        <v-icon>$vuetify.icons.video</v-icon>
+      </v-btn>
+    </template>
+    <v-card >
+      <v-toolbar>
+         <v-icon>$vuetify.icons.video</v-icon>
+          <v-toolbar-title>
+           Example   {{title}}
+          </v-toolbar-title>
+       <v-spacer></v-spacer>
+        <v-btn icon @click="modalValue=false"><v-icon>
+          $vuetify.icons.close
+        </v-icon></v-btn>
+      </v-toolbar>
+      <v-responsive :aspect-ratio="'1.7'">
+       <iframe width="100%" height="100%" :src="'https://www.youtube.com/embed/'+videoId+'?autoplay=1'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
+       </iframe>
+      </v-responsive>
+      <v-card-actions>
+            <v-btn
+              tag="a"
+              disabled
+              text
+              color="deep-purple accent-4"
+            >
+              Read
+            </v-btn>
+            <v-btn
+              tag="a"
+              disabled
+              text
+              color="primary accent-4"
+            >
+              Where to eat
+            </v-btn>
+
+    </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    videoId: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data() {
+     return  {
+        modalValue: false
+     }
+
+  }
+}
+</script>
