c4fe78b5cdf2fb8cf41c813212ed25bac0fc26f8
Dmytro Diachuk <vavilon2@gmail.com>
2020-03-13 10:15:59 +0200

    feat(locations): add new route/page

---
+++ src/core/components/Routes/Locations/index.vue
@@ -0,0 +1,81 @@
+<template>
+
+<v-container  fluid>
+<v-row>
+  <v-col cols="12" sm="6"  md="3" lg="3" xl="3">
+    <v-card>
+      <v-toolbar>
+         Choise
+      </v-toolbar>
+    <v-card-text>
+      <SiteoPlugin
+
+          pluginName="SiteoPluginSelectItems"
+
+          :pluginOptions="{
+            internalApi:citiesLink,
+            label: 'Cities',
+            vComp: {
+              itemValue:'urlId',
+              itemText:'title',
+              hint:'Choose website',
+              rounded: false,
+             }
+        }"
+      >
+      </SiteoPlugin>
+
+
+      <SiteoPlugin
+          pluginName="SiteoPluginSelectItems"
+
+          :pluginOptions="{
+
+            label: 'Values',
+            vComp: {
+              //hint:'Choose website',
+              chips: true,
+              deletableChips:true,
+              rounded: false,
+             }
+        }"
+      >
+      </SiteoPlugin>
+</v-card-text>
+    </v-card>
+
+  </v-col>
+  <v-col cols="12" sm="6"  md="4" lg="4" xl="4">
+    <v-card>
+      <v-toolbar>
+         list
+      </v-toolbar>
+
+    </v-card>
+  </v-col>
+  <v-col cols="12" sm="12"  md="4" lg="5" xl="5">
+    <v-card>
+      <v-toolbar>
+         Map
+      </v-toolbar>
+
+    </v-card>
+  </v-col>
+
+</v-row>
+</v-container>
+
+
+</template>
+
+<script>
+export default {
+
+    computed: {
+       citiesLink() {
+          var siteoId =this.$store.getters['getSiteoConfig']('mainWebsite');
+         return '/apps/'+siteoId+'/locations';
+       }
+    }
+}
+</script>
