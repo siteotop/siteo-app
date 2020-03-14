8655da6820bba1fda6c7584f8c062c95b793defe
Dmytro Diachuk <vavilon2@gmail.com>
2020-03-14 19:58:53 +0200

    feat(Locations): create new card for one loaction

---
+++ src/core/components/Routes/Locations/Cards/Locations.vue
@@ -0,0 +1,21 @@
+<template functional>
+  <v-card>
+    <v-card-title>{{props.title}}</v-card-title>
+    <v-card-text>
+      <div>  {{props.preview}}</div>
+
+    </v-card-text>
+    <v-card-actions>
+        <v-btn >details </v-btn>
+    </v-card-actions>
+
+  </v-card>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
