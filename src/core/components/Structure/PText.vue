1f0e7042384f9f366ad3719dc99b20c665ee1a88
Dmytro Diachuk <vavilon2@gmail.com>
2020-06-11 16:55:06 +0300

    chore(structure): add attribs for components and editing

--- src/core/components/Structure/PText.vue
+++ src/core/components/Structure/PText.vue
@@ -1,9 +1,12 @@
 <template functional>
-<component v-if="props.cntnt.a" :is="props.cnf.a||'div'" :class="data.class">
-  {{parent.$store.state.appInstance.objectActive[props.cntnt.a]}}
-</component>
-<component v-else :is="props.cnf.a||'div'" :class="data.class">
-  {{props.cntnt.t}}
+<component
+  :is="props.cnf.a||'div'"
+  :class="data.class"
+  v-bind="data.attrs"
+  >
+  {{props.cntnt.a?
+    parent.$store.state.appInstance.objectActive[props.cntnt.a]
+    :props.cntnt.t}}
 </component>
 </template>
 <script>
