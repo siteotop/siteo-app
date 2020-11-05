66a7a38f5e9014bec60fc033ded6700e5dc64652
Dmytro Diachuk <vavilon2@gmail.com>
2020-11-05 16:47:15 +0200

    chore:  improve recipe, finish video component

--- src/core/components/Routes/Pages/Functional/Recipe.vue
+++ src/core/components/Routes/Pages/Functional/Recipe.vue
@@ -1,86 +1,128 @@
 <template>
-<v-container>
   <v-responsive class="mx-auto" width="920">
-    <v-row no-gutters>
-    <v-col cols="12" class="text-center">
-      <h1 class="">{{recipe.title}}</h1>
+<v-container>
+
+    <v-row>
+    <v-col cols="12" class="text-center mb-2">
+      <h1 class="text-h2">{{recipe.title}}</h1>
         {{recipe.description}}
+      <ul>
+        <li v-for="(item, i) in menu">
+          <a :href="('#'+item.hash)">{{item.title}}</a>
+        </li>
+      </ul>
     </v-col>
     <v-col cols="12" v-if="recipe.picture">
       <v-img height="500" :src="recipe.picture"></v-img>
     </v-col>
-    <v-col cols="12">
+    <v-col id="ingredients" cols="12">
 
-      <v-card>
-        <v-card-title>Інгрідієнти</v-card-title>
-        <v-data-table
-          v-model="selected"
-          :headers="headers"
-          :items="recipe.jsonStructure.ings"
-          item-key="n"
-          show-select
-          disable-pagination
-          hide-default-footer
-          mobile-breakpoint="0"
-
+      <v-card >
+        <v-card-title><h2 >Інгрідієнти</h2></v-card-title>
+        <v-card-text>
+          <v-list-item-group
+            v-model="settings"
+            multiple
+            active-class=""
           >
-        </v-data-table>
+                <v-list-item v-for="(ing, i) in recipe.jsonStructure.ings">
+                  <template v-slot:default="{ active }">
+                  <v-list-item-action>
+                    <v-checkbox :input-value="active"></v-checkbox>
+                  </v-list-item-action>
+                  <v-list-item-content>
+                      <v-list-item-title>
+                        {{ing.n}}
+                        <span class="grey--text">{{ing.c}} {{ing.t}}</span>
+                      </v-list-item-title>
+                  </v-list-item-content>
+                  </template>
+                </v-list-item>
+            </v-list-item-group>
+        </v-card-text>
       </v-card>
-
-
-    </v-col>
-    <v-col cols="12">
-        Video
     </v-col>
   </v-row>
-  <v-row>
-    <v-col cols="12" v-for="(step, i) in recipe.jsonStructure.sts">
-      <v-card >
+
+  <PYv
+    id="video"
+    v-if="recipe.jsonStructure.v"
+        v-bind="{
+            cntnt: {
+                v: recipe.jsonStructure.v,
+                t: 'Відео рецепт '+ recipe.title
+              },
+            cnf: {}
+          }"
+      >
+    </PYv>
+
+  <h2 id="how-to">Як приготувати </h2>
+  <v-subheader>покроковий рецепт</v-subheader>
+
+
+  <v-row v-for="(step, i) in recipe.jsonStructure.sts">
+    <v-col cols="1">
+      <v-avatar color="grey lighten-3">{{i+1}}</v-avatar>
+    </v-col>
+    <v-col cols="10" >
+      <v-card flat>
          <v-card-title>
-          <v-avatar>{{i+1}}</v-avatar>  {{step.t}}<v-spacer></v-spacer>{{step.c}}
+            {{step.t}}
          </v-card-title>
          <v-card-text>
              {{step.d}}
          </v-card-text>
       </v-card>
     </v-col>
+    <v-col cols="1">
+        {{step.c}}
+    </v-col>
   </v-row>
 
-  </v-responsive>
+
 
 </v-container>
+  </v-responsive>
 </template>
 <script>
 
 import { mapState } from 'vuex';
 export default {
+
   data() {
     return {
-      selected: [],
-      headers: [
-          {
-            text: '',
-            align: 'start',
-            sortable: true,
-            value: 'n',
-          },
-          {
-            text: '',
-            align: 'end',
+      //selected: [],
+      settings:[],
+      menu: [
+        {
+          id:"ings",
+          hash: 'ingredients',
+          title: 'Інгрідієнти'
+        },
+        {
+          id:"v",
+          hash: 'video',
+          title: 'Відео рецепт'
+        },
+        {
+          id:"sts",
+          hash: 'how-to',
+          title: 'Як приготувати'
+        },
+        {
+          id:"tps",
+          hash: 'strava',
+          title: 'Поради'
+        },
+        {
+          id:"d",
+          hash: 'strava',
+          title: 'Про страву'
+        },
 
-            value: 'c',
-          },
+      ],
 
-          {
-            text: '',
-            align: 'start',
-
-            value: 't',
-          },
-
-
-
-        ],
     }
   },
 
